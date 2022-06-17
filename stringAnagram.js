cacbb6827b3e6ca81f2ebfe5191fea5656af17ff
Avishekh Jaiswal <j.avishekh2021@gmail.com>
2022-06-17 15:40:15 +0530

    "added anagram problem with algo"

--- stringAnagram.js
+++ stringAnagram.js
@@ -13,5 +13,33 @@ Satify two conditions to know a string is anagram or not
 
 function isAnagram(str1,str2)
 {
+    if(str1.length!==str2.length)
+    {
+        return `The given ${str1} and ${str2} is not a anagram`
+    }
+
+let counter={}
+    for (let letter of str1)
+    {
+      counter[letter] =(counter[letter]||0)+1;   
+      
+    }
+console.log(counter)
+
+for(let items of str2)
+{
+    if(!counter[items])
+    {
+        return false
+    }
+    else{
+        counter[items] -=1
+    }
+    return "Anagram"
     
 }
+
+}
+
+const ans = isAnagram("Helloabcde","ollehabcde")
+console.log(ans)
