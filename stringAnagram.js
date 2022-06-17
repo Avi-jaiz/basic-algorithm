6f9ce0408710e750b387dc4025bf1d63d2640fd3
Avishekh Jaiswal <j.avishekh2021@gmail.com>
2022-06-17 14:31:03 +0530

    "Added sum problems"

---
+++ stringAnagram.js
@@ -0,0 +1,17 @@
+/* Problem no- 02  */
+/*   
+Find wheather the given string is anagram or not
+
+*/
+/*  Two strings will be said an angram when both the string have same length as well they have same number
+of letters irrespective of positions of the letters */
+
+/*
+Satify two conditions to know a string is anagram or not
+
+*/
+
+function isAnagram(str1,str2)
+{
+    
+}
--- sumequalZero.js
+++ sumequalZero.js
@@ -1,30 +1,82 @@
 // // console.log("This is the first question")
+/*  Write a program to find out the first two pairs whose sum will be Zero   */
 
+// let array =[-5,-4,-3,-2,-1,0,1,2,3,4,5,6]
 
-let array =[-5,-4,-3,-2,-1,0,1,2,3,4,5,6]
-
-function sum(array)
-{
-    for(let i=0;i<array.length;i++)
-    {
-        for(let j=1;j<array.length;j++)
-        {
-            if(array[i]+array[j]===0)
-            {
-                return [array[i],array[j]]
-            }
+// function sum(array)
+// {
+//     for(let i=0;i<array.length;i++)
+//     {
+//         for(let j=1;j<array.length;j++)
+//         {
+//             if(array[i]+array[j]===0)
+//             {
+//                 return [array[i],array[j]]
+//             }
            
+//         }
+//     }
+
+
+// }
+
+// const sum1 = console.log(sum(array))
+
+// console.log(sum1) 
+
+
+/* The above algorith gives O(n^2) time complexity */
+
+
+/* Below algo which gives O(n)  */
+
+
+
+// function isSumZero(array)
+// {
+   
+//    for(let i=0;i<array.length;i++)
+//    {
+//          for(let j=i+1;j<array.length;j++)
+//          {
+//             if(array[i]+array[j]===0)
+//             {
+//                 return [array[i],array[j]]
+//             }
+//          }
+//    }
+// }
+// const ans = isSumZero([-3,-2,-1,0,1,2,3,4])
+// console.log(ans)
+
+
+let array=[-3,-2,-1,0,1,2,3,4]
+function isSumPair(array)
+{
+    let leftIndex=0;
+    let rightIndex=array.length-1;
+
+    while(leftIndex<rightIndex)
+    {
+        let sum= array[rightIndex]+array[leftIndex]
+        if(sum===0)
+        {
+            return [array[leftIndex],array[rightIndex]]
+        }
+
+        else if(sum<0)
+        {
+            leftIndex++
+        }
+        else if(sum>0)
+        {
+            rightIndex--
         }
     }
 
 
 }
 
-const sum1 = console.log(sum(array))
-
-console.log(sum1) 
-
-
-
+console.log(isSumPair(array))
 
 
